44284fe4998122657de541c4642cddb4b2fa2090
Dion Vu <dionvu11@gmail.com>
2024-02-14 14:08:33 -0800

    Created prototypes, removed unnecessary const in function param for size.

--- work_calculator/main.cpp
+++ work_calculator/main.cpp
@@ -4,7 +4,61 @@
 
 using namespace std;
 
-bool readHeightsFromFile(int heightsArr[], const int SIZE) {
+// Prototypes
+bool readHeightsFromFile(int[], int);
+double calcWork(int, int);
+void writeResultsToFile(int[], int[], int);
+void insertionSortRecursive(int[], int);
+
+int main() {
+  const int SIZE = 5;
+  int heights[SIZE];
+  int work[SIZE];
+
+  readHeightsFromFile(heights, SIZE);
+
+  insertionSortRecursive(heights, SIZE);
+
+  for (int i = 0; i < SIZE; i++) {
+    work[i] = calcWork(heights[i], 50);
+  }
+
+  writeResultsToFile(heights, work, SIZE);
+}
+
+void insertionSortRecursive(int arr[], int SIZE) {
+  if (SIZE <= 1)
+    return;
+
+  insertionSortRecursive(arr, SIZE - 1);
+
+  int last = arr[SIZE - 1];
+  int cur = SIZE - 2;
+
+  while (cur >= 0 && arr[cur] > last) {
+    arr[cur + 1] = arr[cur];
+    cur--;
+  }
+  arr[cur + 1] = last;
+}
+
+void writeResultsToFile(int heightsArr[], int workArr[], int SIZE) {
+  ofstream file("work_results.txt");
+  for (int i = 0; i < SIZE; i++) {
+    file << heightsArr[i] << " " << workArr[i] << '\n';
+  }
+  file.close();
+}
+
+double calcWork(int lowerBound, int upperBound) {
+  double lower = (40 * pow(lowerBound, 3) / 3) + (10 * pow(lowerBound, 2) / 2) +
+                 (300 * lowerBound);
+  double upper = (40 * pow(upperBound, 3) / 3) + (10 * pow(upperBound, 2) / 2) +
+                 (300 * upperBound);
+  return upper - lower;
+}
+
+bool readHeightsFromFile(int heightsArr[], int SIZE) {
   ifstream file("heights.txt");
 
   if (!file) {
@@ -18,34 +72,4 @@ bool readHeightsFromFile(int heightsArr[], const int SIZE) {
   file.close();
 
   return true;
-}
-
-double calcWork(int lowerBound, int upperBound) {
-  double lower = (40 * pow(lowerBound, 3) / 3) + (10 * pow(lowerBound, 2) / 2) +
-                 (300 * lowerBound);
-  double upper = (40 * pow(upperBound, 3) / 3) + (10 * pow(upperBound, 2) / 2) +
-                 (300 * upperBound);
-  return upper - lower;
-}
-
-void writeResultsToFile(int heightsArr[], int workArr[], const int SIZE) {
-  ofstream file("work_results.txt");
-  for (int i = 0; i < SIZE; i++) {
-    file << heightsArr[i] << " " << workArr[i] << '\n';
-  }
-  file.close();
-}
-
-int main() {
-  const int SIZE = 5;
-  int heights[SIZE];
-  int work[SIZE];
-
-  readHeightsFromFile(heights, SIZE);
-
-  for (int i = 0; i < SIZE; i++) {
-    work[i] = calcWork(heights[i], 50);
-  }
-
-  writeResultsToFile(heights, work, SIZE);
 }
